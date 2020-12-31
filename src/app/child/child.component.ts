f023327f341d8f0692fed6ef5e61ff53f8384d0f
vigamage <wirajlakshitha@gmail.com>
2020-12-31 20:21:25 +0530

    changes

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -17,24 +17,28 @@ export class ChildComponent implements OnInit, OnChanges {
   ngOnInit(): void {
 
     this._initializeForm();
-
-    this.dataForm.statusChanges.pipe(filter(() => this.dataForm.valid))
-      .subscribe(() => alert('Form is valid'));
+    this._setupListner();
 
   }
 
   _initializeForm() {
 
     this.dataForm = this._formBuilder.group({
-      name: [this.formDataFromParent.name, [Validators.required]],
-      age: [this.formDataFromParent.age, [Validators.required, Validators.min(10), Validators.max(50)]]
+      name: [this.formDataFromParent?.name, [Validators.required]],
+      age: [this.formDataFromParent?.age, [Validators.required, Validators.min(10), Validators.max(50)]]
     });
 
+  }
+
+  _setupListner() {
+    this.dataForm.statusChanges.pipe(filter(() => this.dataForm.valid))
+      .subscribe(() => alert('Form is valid'));
   }
 
   ngOnChanges(changes: SimpleChanges) {
     if (changes.formDataFromParent) {
       this._initializeForm();
+      this._setupListner();
     }
   }
 
