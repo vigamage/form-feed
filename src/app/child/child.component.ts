be119b6638c105730999df8a9da641079876b961
vigamage <wirajlakshitha@gmail.com>
2020-12-31 20:11:38 +0530

    initial commit

---
+++ src/app/child/child.component.ts
@@ -0,0 +1,41 @@
+import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { filter } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styleUrls: ['./child.component.css']
+})
+export class ChildComponent implements OnInit, OnChanges {
+
+  @Input() formDataFromParent: any;
+  public dataForm: FormGroup;
+
+  constructor(private _formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+
+    this._initializeForm();
+
+    this.dataForm.statusChanges.pipe(filter(() => this.dataForm.valid))
+      .subscribe(() => alert('Form is valid'));
+
+  }
+
+  _initializeForm() {
+
+    this.dataForm = this._formBuilder.group({
+      name: [this.formDataFromParent.name, [Validators.required]],
+      age: [this.formDataFromParent.age, [Validators.required, Validators.min(10), Validators.max(50)]]
+    });
+
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.formDataFromParent) {
+      this._initializeForm();
+    }
+  }
+
+}
